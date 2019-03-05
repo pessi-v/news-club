2e9021797d5c9e0e859033e085c18454fc76dff6
gianmaria scioti <jimmyscioti@gmail.com>
2019-03-05 15:27:32 +0100

    subscription new

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,7 +1,14 @@
 class SubscriptionsController < ApplicationController
 
   def new
+    @plan1 = Plan.find(1)
+    @plan2 = Plan.find(2)
     @subscription = Subscription.new
+  end
+
+  def create
+    @subscription = Subscription.new
+
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ plan1 = Plan.create(amount: 10, price_cents: 500)
+ plan2 = Plan.create(amount: 25, price_cents: 1000)
