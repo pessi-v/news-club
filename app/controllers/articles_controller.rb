d25460c984d1bfc86912a945da6f75774083408a
Pessi Virta <haamutili@gmail.com>
2019-03-05 15:13:15 +0100

    implemented basic api call for news api, started working on adding articles to db, made some cta

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
-class ArticlesController < ApplicationController
+require 'open-uri'
+require 'json'
 
+class ArticlesController < ApplicationController
+  def index
+    fetch_articles
+  end
 
   private
 
@@ -7,4 +12,24 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :author, :date, :source, :tag_list) ## Rails 4 strong params usage
   end
 
+  def fetch_articles
+    url = 'https://newsapi.org/v2/everything?'\
+      'q=Berlin&'\
+      'from=2019-03-05&'\
+      'sortBy=popularity&'\
+      "apiKey=#{ENV['NEWSAPI_API_KEY']}"
+
+    req = open(url)
+    response_body = req.read
+    articles_json = JSON.parse(response_body)
+    articles_array = articles_json["articles"]
+    last_articles = []
+    articles_array.each do |a|
+      if article = Article.find_by(url: a["url"]) #check if article is already in the database
+        last_articles << article
+      else article = Article.new(title: a["title"], author: a["author"], source: a["source"]["name"], url: a["url"], date: a["publishedAt"], content: a["content"], image: a["urlToImage"])
+        last_articles << article
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_164726) do
+ActiveRecord::Schema.define(version: 2019_03_05_140423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2019_03_04_164726) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title", null: false
+    t.string "url", null: false
+    t.text "content", null: false
+    t.string "image"
   end
 
   create_table "bookmarks", force: :cascade do |t|
