ae61dda905aaae3ad48468852605b9323557d385
Kirsty Simmonds <kirstysimmonds@icloud.com>
2019-03-06 16:26:18 +0100

    finishing button component

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,7 +33,7 @@ class ArticlesController < ApplicationController
     articles_array = articles_json["articles"]
     last_articles = []
     articles_array.each do |a|
-      article = Article.find_by(url: a["url"])
+      article = Article.find_by(title: a["title"])
       unless article # check if article is already in the database
         article = Article.new(title: a["title"], author: a["author"] || a["source"], source: a["source"]["name"], url: a["url"], date: a["publishedAt"], content: a["content"] || "no content available", image: a["urlToImage"], description: a["description"])
         article.save!
