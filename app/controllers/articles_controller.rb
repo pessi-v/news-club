66abf01dd967bd99f65f833db988cc3a4ff6af1d
gianmaria scioti <jimmyscioti@gmail.com>
2019-03-08 15:32:50 +0100

    homepahe

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,8 +24,8 @@ class ArticlesController < ApplicationController
   end
 
   def home
-    @all_user_articles
-    Article.where()
+    #@all_user_articles = Article.all
+    @all_user_articles = Article.tagged_with(current_user.publication_list, any: true)
   end
 
   private
@@ -63,3 +63,5 @@ class ArticlesController < ApplicationController
     last_articles
   end
 end
+
+
