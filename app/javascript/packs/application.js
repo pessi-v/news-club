1a7f16073e50098d02bcb5debcd6530314af7ef2
Kirsty Simmonds <kirstysimmonds@icloud.com>
2019-03-11 16:47:53 +0100

    enlarged images on show page and installed applause button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,7 @@
 import "bootstrap";
+import "applause-button/dist/applause-button";
+import "applause-button/dist/applause-button.css";
+
 // import "navbar.js";
 
 // import navbar.js js function here
