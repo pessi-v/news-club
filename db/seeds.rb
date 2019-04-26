ef1fc986bfdadedf6f141d33fefc266b5047047e
Pessi Virta <haamutili@gmail.com>
2019-04-26 12:11:42 +0200

    seed: added chack for creating plans only if plans don't exist yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 
-plan1 = Plan.create(amount:10,price_cents:500)
-plan2 = Plan.create(amount:25,price_cents:1000)
+
+plan1 = Plan.create(amount:10,price_cents:500) if Plan.first == nil
+plan2 = Plan.create(amount:25,price_cents:1000) if Plan.second == nil
 # plan3 = Plan.create(amount:500,price_cents:1000)
 require 'date'
 
